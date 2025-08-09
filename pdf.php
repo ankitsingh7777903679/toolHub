71bb8839070a4f1f88f63afd02811d0663c3b5d0
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-08-09 17:54:18 +0530

    pdf page

---
+++ pdf.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="pdf.css">
+</head>
+
+<body>
+    <header> <?php include 'navbar.php' ?></header>
+    <div>
+        <div class="container pdf_tool_head d-flex flex-column justify-content-center align-items-center">
+            <h1 class="mb-4">Pdf Tools</h1>
+            <p>Free Online PDF Tools</p>
+        </div>
+        <div class="container pdf_tool_body d-flex justify-content-center align-items-center mt-4">
+            <form class="d-flex col-lg-6 search-tool mb-2 mb-lg-0 align-items-center shadow shadow-lg" role="search">
+                <i class="fa-solid fa-magnifying-glass" style="color: #8B8D8F;"></i>
+                <input class="search-tool-input form-control form-control-sm border border-0" type="search" placeholder="Search" aria-label="Search" style="background: none; color: #8B8D8F; ">
+                <button class="search-tool-btn btn" type="submit">search</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
